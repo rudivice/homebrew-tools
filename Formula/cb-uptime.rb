2936d276d8e3b887b7c9834575624947b3c5a7e9
Rüdiger Weiß <ruediger@mac.com>
2026-08-19 15:27:11 +0200

    cb-uptime: bump to v0.2.4

--- Formula/cb-uptime.rb
+++ Formula/cb-uptime.rb
@@ -68,10 +68,10 @@ end
 class CbUptime < Formula
   desc "Synthetic availability monitoring for the Corporate Benefits platforms"
   homepage "https://github.com/rudivice/cb-uptime"
-  url "https://github.com/rudivice/cb-uptime/releases/download/v0.2.3/cb-uptime-0.2.3-arm64.tar.gz",
+  url "https://github.com/rudivice/cb-uptime/releases/download/v0.2.4/cb-uptime-0.2.4-arm64.tar.gz",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.2.3"
-  sha256 "586934c699084f0c2d1e0dc2c8cc2718d491b8fd992d10fdd0e4dac3a72894dc"
+  version "0.2.4"
+  sha256 "1e0c70f65be4749f6422858eef0c2371d2af39b2051c476fdcea0a6b9500ce91"
 
   depends_on arch: :arm64
   depends_on macos: :sonoma
